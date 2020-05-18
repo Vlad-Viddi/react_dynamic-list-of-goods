eaadd4b0e5314ad0d65cb940e0e94810a1728e21
Vlad <oneviddi@gmail.com>
2020-05-18 22:28:30 +0300

    Updates

---
+++ precache-manifest.57b98bbc9bbb948edefe70d38099bd7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cd4262f6490cea912d25809dbe279c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a630eebee25839b88b4",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "acf890f14034b7345c62",
+    "url": "./static/js/2.84b77ff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.84b77ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a630eebee25839b88b4",
+    "url": "./static/js/main.b0619b4f.chunk.js"
+  },
+  {
+    "revision": "cc951e334f15251cfce1",
+    "url": "./static/js/runtime-main.88190073.js"
+  }
+]);
